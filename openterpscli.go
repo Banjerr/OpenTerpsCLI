11cded1fa5470914255db08c1a21b1f47ff94adc
Ben Redden <benjamminredden@gmail.com>
2020-11-08 10:11:29 -0500

    stylistic updates

--- openterpscli.go
+++ openterpscli.go
@@ -1,48 +1,3 @@
-// package main
-
-// import (
-// 	"fmt"
-// 	"log"
-// 	"os"
-
-// 	"github.com/urfave/cli/v2"
-// )
-
-// func main() {
-// 	app := &cli.App{
-// 		Name:  "hello",
-// 		Usage: "greet the user",
-// 		Action: func(c *cli.Context) error {
-// 			fmt.Println(`
-//                  |
-//                 |.|
-//                |\./|
-//                |\./|
-// .              |\./|              .
-// \^.\          |\\.//|          /.^/
-//  \--.|\       |\\.//|       /|.--/
-//    \--.| \    |\\.//|    / |.--/
-//     \---.|\    |\./|    /|.---/
-//        \--.|\  |\./|  /|.--/
-//           \ .\  |.|  /. /
-// _ -_^_^_^_-  \ \\ // /  -_^_^_^_- _
-//   - -/_/_/- ^ ^  |  ^ ^ -\_\_\- -
-//             /-./ | \.-\
-//            /-/   |   \-\
-// 	  ;|'         '|;
-// -=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-
-// 	  OpenTerpsCLI
-// 			`)
-// 			return nil
-// 		},
-// 	}
-
-// 	err := app.Run(os.Args)
-// 	if err != nil {
-// 		log.Fatal(err)
-// 	}
-// }
-
 package main
 
 import (
@@ -54,32 +9,63 @@ import (
 )
 
 func main() {
+	cli.AppHelpTemplate = `
+-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-
+                 |
+                |.|
+               |\./|
+               |\./|
+.              |\./|              .
+\^.\          |\\.//|          /.^/
+ \--.|\       |\\.//|       /|.--/
+   \--.| \    |\\.//|    / |.--/
+    \---.|\    |\./|    /|.---/
+       \--.|\  |\./|  /|.--/
+          \ .\  |.|  /. /
+_ -_^_^_^_-  \ \\ // /  -_^_^_^_- _
+  - -/_/_/- ^ ^  |  ^ ^ -\_\_\- -
+            /-./ | \.-\
+           /-/   |   \-\
+	        ;|'         '|;
+-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-
+ _____  ____  ____  _  _  ____  ____  ____  ____  ___     ___  __    ____ 
+(  _  )(  _ \( ___)( \( )(_  _)( ___)(  _ \(  _ \/ __)   / __)(  )  (_  _)
+ )(_)(  )___/ )__)  )  (   )(   )__)  )   / )___/\__ \  ( (__  )(__  _)(_ 
+(_____)(__)  (____)(_)\_) (__) (____)(_)\_)(__)  (___/   \___)(____)(____)
+-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-
+WEBSITE: https://openterps.countryfriedcoders.me
+-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-
+SUPPORT: benjamminredden@gmail.com
+-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-
+-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-
+NAME:
+   {{.Name}} - {{.Usage}}
+USAGE:
+   {{.HelpName}} {{if .VisibleFlags}}[global options]{{end}}{{if .Commands}} command [command options]{{end}} {{if .ArgsUsage}}{{.ArgsUsage}}{{else}}[arguments...]{{end}}
+   {{if len .Authors}}
+AUTHOR:
+   {{range .Authors}}{{ . }}{{end}}
+   {{end}}{{if .Commands}}
+COMMANDS:
+{{range .Commands}}{{if not .HideHelp}}   {{join .Names ", "}}{{ "\t"}}{{.Usage}}{{ "\n" }}{{end}}{{end}}{{end}}{{if .VisibleFlags}}
+GLOBAL OPTIONS:
+   {{range .VisibleFlags}}{{.}}
+   {{end}}{{end}}{{if .Copyright }}
+COPYRIGHT:
+   {{.Copyright}}
+   {{end}}{{if .Version}}
+VERSION:
+   {{.Version}}
+   {{end}}
+`
+
 	app := &cli.App{
 		Commands: []*cli.Command{
 			{
 				Name:  "info",
-				Usage: "greet the user",
+				Usage: "provides useful informational about the OpenTerps CLI 🍁",
 				Action: func(c *cli.Context) error {
-					fmt.Println(`
-                 |       
-                |.|      
-               |\./|     
-               |\./|
-.              |\./|              .     
-\^.\          |\\.//|          /.^/      
- \--.|\       |\\.//|       /|.--/          
-   \--.| \    |\\.//|    / |.--/
-    \---.|\    |\./|    /|.---/     
-       \--.|\  |\./|  /|.--/        
-          \ .\  |.|  /. /          
-_ -_^_^_^_-  \ \\ // /  -_^_^_^_- _   
-  - -/_/_/- ^ ^  |  ^ ^ -\_\_\- -      
-            /-./ | \.-\
-           /-/   |   \-\              
-	  ;|'         '|;
--=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-
-	  OpenTerpsCLI
-			`)
+					fmt.Println(``)
 					return nil
 				},
 			},
@@ -88,6 +74,7 @@ _ -_^_^_^_-  \ \\ // /  -_^_^_^_- _
 				Usage: "looks up cannabis strain by name",
 				Action: func(c *cli.Context) error {
 					fmt.Println("look up strain name: ", c.Args().First())
+					fmt.Printf("getting info on strain: %q", c.Args().First())
 					return nil
 				},
 			},
